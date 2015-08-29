d4ad992837f3919dff5a4802269e037a5e3462c6
Juanjo Torroglosa <jjtorroglosa@gmail.com>
2015-08-29 14:48:36 +0200

    Test application

--- test/Application/ApplicationTest.php
+++ test/Application/ApplicationTest.php
@@ -10,12 +10,34 @@
 
 namespace Cliphar\Application;
 
+use Cliphar\Application\Fixtures\TestApplication;
 use PHPUnit_Framework_TestCase;
+use Symfony\Component\Console\Input\InputInterface;
 
 class ApplicationTest extends PHPUnit_Framework_TestCase
 {
     public function testApplication()
     {
-        $this->fail("Fix the getInstance call from cmd function");
+        $_SERVER['argv'] = array('TestApplication','anycommand', '-o', 'argumentpassed');
+        $app = TestApplication::getInstance();
+        $this->givenACommandDefinition($option, $argument);
+
+        $exitCode = $app->run();
+
+        $this->assertTrue($option);
+        $this->assertEquals('argumentpassed', $argument);
+        $this->assertEquals(0, $exitCode);
+    }
+
+    private function givenACommandDefinition(& $option, & $argument)
+    {
+        cmd("anycommand", '[option|o] <argument>="Value"',
+
+            function (InputInterface $inputInterface) use (& $option, & $argument) {
+                $option = $inputInterface->getOption('option');
+                $argument = $inputInterface->getArgument('argument');
+            }
+
+        );
     }
 }
